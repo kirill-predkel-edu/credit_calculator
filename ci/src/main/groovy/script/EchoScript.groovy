4c61367fdd26c6f779cb6c025419ea63aff67fa1
kirill-predkel <kirill.predkel@idfinance.com>
2022-12-04 21:54:47 +0300

    add multimodule structure

--- ci/src/main/groovy/script/EchoScript.groovy
+++
@@ -1,7 +0,0 @@
-package script
-
-class EchoScript {
-    def getBuildString() {
-        return "build jenkins file is running"
-    }
-}
--- feature/src/main/java/org/example/Main.java
+++ feature/src/main/java/org/example/Main.java
@@ -1,10 +1,8 @@
 package org.example;
 
-import script.EchoScript;
 
 public class Main {
     public static void main(String[] args) {
-        EchoScript script = new EchoScript();
         System.out.println("Hello world!");
     }
 
