ef97c8d3ed618269b9a5c2fea9ae776dcf696a3b
kirill-predkel <kirill.predkel@idfinance.com>
2022-12-04 22:08:16 +0300

    add multimodule structure

--- ci/src/main/groovy/script/StaticScript.groovy
+++ ci/src/main/groovy/script/StaticScript.groovy
@@ -1,12 +1,9 @@
 package script
 
-import groovy.transform.Field
-
 static def getStaticString() {
     return "test jenkins file is running"
 }
 
-@Field
 String name = "test jenkins file is running"
 
 return this
