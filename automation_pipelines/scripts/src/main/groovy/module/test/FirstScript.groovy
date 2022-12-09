996b331f42bed5c0022c73f993c284a491b92cd7
kirill-predkel <kirill.predkel@idfinance.com>
2022-12-09 17:26:08 +0300

    add multimodule structure

--- automation_pipelines/scripts/src/main/groovy/module/test/FirstScript.groovy
+++ automation_pipelines/scripts/src/main/groovy/module/test/FirstScript.groovy
@@ -1,4 +1,4 @@
-package module
+package module.test
 
 class FirstScript implements Serializable {
     def scriptField
--- automation_pipelines/scripts/src/main/groovy/module/test/SecondScript.groovy
+++ automation_pipelines/scripts/src/main/groovy/module/test/SecondScript.groovy
@@ -1,4 +1,4 @@
-package module
+package module.test
 
 import groovy.transform.Field
 
